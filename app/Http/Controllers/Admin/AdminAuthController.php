f3250112f51ee4823aa87b7abaa0fdc4c6d885ec
haikaljs <haikaljavascript@gmail.com>
2024-03-18 02:55:40 +0800

    Multi Auth - Create view and controller for admin auth

---
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;
+
+class AdminAuthController extends Controller
+{
+    function login() : View
+    {
+        return view('admin.auth.login');
+    }
+}
---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    function index() : View{
+        return view('admin.dashboard.index');
+    }
+}
