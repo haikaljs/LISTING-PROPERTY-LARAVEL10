956eee2420e1c502156f5475678aaae28d83a60c
haikaljs <haikaljavascript@gmail.com>
2024-03-18 09:42:10 +0800

    Admin setup - admin login functionality

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -84,21 +84,26 @@
                             </div>
 
                             <div class="card-body">
-                                <form method="POST" action="#" class="needs-validation" novalidate="">
+                                <form method="POST" action="{{ route('login') }}" class="needs-validation"
+                                    novalidate="">
+                                    @csrf
                                     <div class="form-group">
                                         <label for="email">Email</label>
                                         <input id="email" type="email" class="form-control" name="email"
-                                            tabindex="1" required autofocus>
+                                            value="{{ old('email') }}" tabindex="1" required autofocus>
                                         <div class="invalid-feedback">
-                                            Please fill in your email
+                                            please fill in your email
                                         </div>
+                                        @if ($errors->first('email'))
+                                            <code>{{ $errors->first('email') }}</code>
+                                        @endif
                                     </div>
 
                                     <div class="form-group">
                                         <div class="d-block">
                                             <label for="password" class="control-label">Password</label>
                                             <div class="float-right">
-                                                <a href="auth-forgot-password.html" class="text-small">
+                                                <a href="{{ route('admin.password.request') }}" class="text-small">
                                                     Forgot Password?
                                                 </a>
                                             </div>
@@ -108,15 +113,21 @@
                                         <div class="invalid-feedback">
                                             please fill in your password
                                         </div>
+                                        @if ($errors->first('password'))
+                                            <code>{{ $errors->first('password') }}</code>
+                                        @endif
                                     </div>
+
+
 
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox">
                                             <input type="checkbox" name="remember" class="custom-control-input"
-                                                tabindex="3" id="remember-me">
-                                            <label class="custom-control-label" for="remember-me">Remember Me</label>
+                                                tabindex="3" id="remember_me">
+                                            <label class="custom-control-label" for="remember_me">Remember Me</label>
                                         </div>
                                     </div>
+
 
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
@@ -124,29 +135,13 @@
                                         </button>
                                     </div>
                                 </form>
-                                <div class="text-center mt-4 mb-3">
-                                    <div class="text-job text-muted">Login With Social</div>
-                                </div>
-                                <div class="row sm-gutters">
-                                    <div class="col-6">
-                                        <a class="btn btn-block btn-social btn-facebook">
-                                            <span class="fab fa-facebook"></span> Facebook
-                                        </a>
-                                    </div>
-                                    <div class="col-6">
-                                        <a class="btn btn-block btn-social btn-twitter">
-                                            <span class="fab fa-twitter"></span> Twitter
-                                        </a>
-                                    </div>
-                                </div>
+
 
                             </div>
                         </div>
-                        <div class="mt-5 text-muted text-center">
-                            Don't have an account? <a href="auth-register.html">Create One</a>
-                        </div>
+
                         <div class="simple-footer">
-                            Copyright &copy; Stisla 2018
+                            Copyright &copy; haikalrozhan.com {{ date('Y') }}
                         </div>
                     </div>
                 </div>
