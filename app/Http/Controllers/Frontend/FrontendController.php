ddc3b9cf91f2166ba69f6c2bed2ea6e230a0cc74
haikaljs <haikaljavascript@gmail.com>
2024-03-21 08:10:09 +0800

    Frontend - Display data in home section

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Frontend;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Hero;
 
 class FrontendController extends Controller
 {
     function index() : View{
-
-        return view('frontend.home.index');
+        $hero = Hero::first();
+        return view('frontend.home.index', compact('hero'));
     }
 }
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -26,7 +26,7 @@
         </ul>
 
         <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+            <a href="{{ route('home') }}" target="_blank" class="btn btn-primary btn-lg btn-block btn-icon-split">
                 <i class="fas fa-rocket"></i> Frontend
             </a>
         </div>
